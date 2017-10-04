8631a4637d1545f91f60739aa34ad8b880a12115
elizabete <elizabetereiss@gmail.com>
2017-10-04 02:57:42 -0300

    commit inicial

---
+++ JenaTutorial/src/tutorial/HelloRDFWorld.java
@@ -0,0 +1,26 @@
+package tutorial;
+
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.Resource;
+
+public class HelloRDFWorld {
+	
+	public static void main(String[] args) {
+		
+		Model m = ModelFactory.createDefaultModel();
+		
+		String NS = "http://example.com/test/";
+
+		Resource r = (Resource) m.createResource(NS + "r");
+		Property p = m.createProperty(NS + "p");
+
+		r.addProperty(p, "hello world", XSDDatatype.XSDstring);
+
+		m.write(System.out, "Turtle");
+		
+	}
+
+}
